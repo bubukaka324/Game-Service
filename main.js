ef2f0b60aef76dbc9ce0cd8c471e0098d329a5d5
bubukaka324 <danpavlov1213@gmail.com>
2025-02-25 19:03:53 +0200

    cart

--- main.js
+++ main.js
@@ -18,13 +18,53 @@ const games=[
 
 let cartButton = document.querySelector(".cartbutton")
 let buttons = document.querySelectorAll(".buy-button")
+let cartcontainer = document.querySelector(".cartpanel")
+
+cartcontainer.hidden = true
 
 buttons.forEach((button,index) => {
     button.addEventListener("click", ()=>{
-        cart.push(games[index])
+        if(cart.find(element => element.name == games[index].name) ===-1 || cart.find(element => element.name == games[index].name) === undefined){
+        cart.push({name:games[index].name,price:games[index].price,amount:1})}
+        else{cart.find(element => element.name == games[index].name).price += games[index].price
+        cart.find(element => element.name == games[index].name).amount+=1};
+        updateCart()
     })
 
 });
 cartButton.addEventListener("click", ()=>{
     console.log(cart)
+    if(cartcontainer.hidden == true){
+        cartcontainer.hidden = false
+    }
+    else
+    cartcontainer.hidden = true
 })
+function updateCart(){
+    cartcontainer.innerHTML = ""
+    let title = document.createElement("p")
+    title.innerText = "Your cart"
+    cartcontainer.appendChild(title)
+    cart.forEach((element) => {
+        let gamecontainer = document.createElement("div");
+        gamecontainer.className = "game-container"
+        let gamename = document.createElement("p");
+        let gameprice = document.createElement("p");
+        let gameamount = document.createElement("p");
+        let removebutton = document.createElement("button");
+        gamename.innerText = element.name
+        gameprice.innerText = element.price
+        gameamount.innerText = element.amount
+        removebutton.innerText = "REMOVE";
+        removebutton.addEventListener("click", ()=>{
+            gamecontainer.remove();
+            let index = cart.indexOf(element)
+            cart.splice(index, 1)
+        })
+        gamecontainer.appendChild(gamename)
+        gamecontainer.appendChild(gameprice)
+        gamecontainer.appendChild(gameamount)
+        gamecontainer.appendChild(removebutton)
+        cartcontainer.appendChild(gamecontainer)
+    })
+}
